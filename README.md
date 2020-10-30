c6f7869f647996a11a13cbb21ef1c3c94a43a183
Nicolas Williams <ndwill@umich.edu>
2020-10-30 12:59:50 -0700

    Send messages in channel

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # ReminderBot
 A WolverineSoft Studio Discord bot that gives us reminders!
 
-### TODO
+### Components
+* Stores reminders
+* Sends messages when reminders are due
+* 
+
+### MVP
+* Sends a message at a specific time
+### WIP
+* run the bot as a background process
 * 
 
 #### Future features
 * Allow creation & configuration of a reminder in Discord
-* 
+* Connect to AWS or means of robust continuous execution
--- bot.py
+++ bot.py
@@ -3,18 +3,42 @@
 # A Discord bot for reminders in WolverineSoft Studio
 # Created by Nicolas Williams, 10/30/2020
 
+# Webpage with instructions on Discord bots
+# https://realpython.com/how-to-make-a-discord-bot-python/
+
 import os
 
 import discord
 from dotenv import load_dotenv
 
+from discord.ext import commands
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+SERVER = os.getenv('DISCORD_SERVER')
 
 client = discord.Client()
 
+# An event is something that happens on Discord that you can use to trigger a
+# reaction in your code. Your code will listen for and then respond to events.
+# TODO: Allow reminders to be created based on createReminder command
+#   - define message, channel, mention targets, reactions, frequency & time
+
+# TODO: Send a message in the discord with the reminder
+
+# TODO: React to the reminder with relevant reactions
+
 @client.event
 async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+    for guild in client.guilds:
+        if guild.name == SERVER:
+            break
+
+    print(
+            f"{client.user} is connected to the following server:\n"
+            f"{guild.name} (id: {guild.id})"
+            )
+    channel = discord.utils.get(guild.text_channels, name="general")
+    await channel.send('This is a test message')
 
 client.run(TOKEN)
