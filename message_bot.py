aecc22605f9871f09936e6fadca2e3465255dcda
Nicolas Williams <ndwill@umich.edu>
2020-11-02 16:54:10 -0800

    Handles all json file errors

--- message_bot.py
+++ message_bot.py
@@ -13,6 +13,15 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+
+class ReminderError(Exception):
+    pass
+
+
+class GuildError(Exception):
+    pass
+
+
 # Connect to Discord
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -24,7 +33,7 @@ bot = commands.Bot(command_prefix="!")
 async def on_ready():
     if len(sys.argv) == 1:  # No parameters were given
         print_error("No parameters given to script, exiting...")
-        # TODO: Print usage
+        # TODO: Print correct usage of the script
         close_bot()
         return
 
@@ -33,9 +42,11 @@ async def on_ready():
     try:
         reminder = acquire_reminder(REMINDER_FILE)
 
+        validate_reminder(reminder)
+
         await send_reminder(reminder)
     except:
-        print_error(f"An error occured, closing program.")
+        print_error(f"Errors have occured. Closing program.")
         await close_bot()
         return
 
@@ -51,9 +62,6 @@ async def on_ready():
 
 # Returns reminder loaded from given reminder json file
 # FIXME: Consider using fcntl.flock() to avoid race conditions & file corruption
-# FIXME: errors for when file is...
-# - doesn't exist
-# - can't be parsed
 def acquire_reminder(reminder_file: str):
     try:
         with open(reminder_file) as json_file:
@@ -62,9 +70,26 @@ def acquire_reminder(reminder_file: str):
     except IOError as e:
         print_error(f"ERROR: File {reminder_file} doesn't exist.")
         raise e
+    except json.decoder.JSONDecodeError as e:
+        print_error(f"PARSE_ERROR: file '{reminder_file}' could not be decoded.")
+        raise e
 
-    # TODO: Validation of json file
+    # TODO: Validation of data acquired from JSON file
     return reminder
+
+
+# TODO: Write this function
+# Validates the information within a reminder acquired from a json file
+def validate_reminder(reminder: dict):
+    # validate all required keys exist
+    required_keys = ["server", "channel", "message"]
+    for prop_key in required_keys:
+        if prop_key not in reminder:
+            # missing a required property key
+            print_error(
+                f"REMINDER_ERROR: Reminder json file is missing the key '{prop_key}'"
+            )
+            raise REMINDER_ERROR
 
 
 # Sends given reminder message to appropriate channel
@@ -73,9 +98,20 @@ def acquire_reminder(reminder_file: str):
 # - channel doesn't exist
 # - message can't be sent in channel
 async def send_reminder(reminder):
+    # TODO: Try to make this more pretty / less try statements
     server = discord.utils.get(bot.guilds, name=reminder["server"])
+    if server == None:
+        print_error(f"ERROR with finding server '{reminder['server']}'")
+        raise GuildError
     channel = discord.utils.get(server.text_channels, name=reminder["channel"])
-    await channel.send(reminder["message"])
+    if channel == None:
+        print_error(f"ERROR with finding channel'{reminder['channel']}'")
+        raise GuildError
+    try:
+        await channel.send(reminder["message"])
+    except:
+        print_error(f"ERROR with sending message in channel '{reminder['channel']}'")
+        raise
 
 
 def print_error(message):
